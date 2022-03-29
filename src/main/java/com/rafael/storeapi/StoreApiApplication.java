e1a2b0daccad1752175e4fb76d9395541dbab4bf
Rafael Oliveira <raf100guy@gmail.com>
2022-03-29 15:30:20 -0300

    Initial commit

---
+++ src/main/java/com/rafael/storeapi/StoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.storeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiApplication.class, args);
+	}
+
+}
